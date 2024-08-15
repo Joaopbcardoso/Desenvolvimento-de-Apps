641cd81db032a404cf0c413d0e8d49aa51059cbf
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-08-15 08:19:15 -0300

    apps

---
+++ app/(tabs)/lista-tarefa/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
+
+export default listaTarefa = () => {
+    return <View style={styles.container}>
+        <Text>Hello World!</Text>
+    </View>
+}
