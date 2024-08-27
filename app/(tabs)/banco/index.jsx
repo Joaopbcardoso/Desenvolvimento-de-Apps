f55d51e5be7089e19b52f364111491ace973c1b0
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-08-27 12:16:23 -0300

    style

--- app/(tabs)/banco/index.jsx
+++ app/(tabs)/banco/index.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
-import { View, Text, Pressable, TextInput, StyleSheet, Image } from 'react-native';
+import { View, Text, Pressable, TextInput, StyleSheet, Image, Modal, Alert } from 'react-native';
 
 const App = () => {
     const [number, setNumber] = useState(7320.92);
     const [valor, setValor] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [operationType, setOperationType] = useState('');
 
-    const sacar = () => {
-        const valorComMulta = Number(valor) * 1.025; // Aplica 2,5% de multa
-        const novoSaldo = Number(number) - valorComMulta;
+    const operacao = (type) => {
+        const multiplier = type === 'sacar' ? 1.025 : 1.01;
+        const valorComMulta = Number(valor) * multiplier;
+        const novoSaldo = type === 'sacar' ? Number(number) - valorComMulta : Number(number) + valorComMulta;
+
         setNumber(novoSaldo);
+        setIsModalOpen(false); 
     };
 
-    const depositar = () => {
-      const valorComMulta = Number(valor) * 1.01; // Aplica 2,5% de multa
-      const novoSaldo = Number(number) + valorComMulta;
-      setNumber(novoSaldo);
-  };
+    const openModal = (type) => {
+        setOperationType(type);
+        setIsModalOpen(true);
+    };
 
     return (
         <View style={styles.container}>
@@ -39,9 +43,34 @@ const App = () => {
                 />
             </View>
             <View style={styles.options}>
-                <Pressable onPress={sacar}><Text style={styles.botao}>Sacar</Text></Pressable>
-                <Pressable onPress={depositar}><Text style={styles.botao}>Depositar</Text></Pressable>
+                <Pressable onPress={() => openModal('sacar')}><Text style={styles.botao}>Sacar</Text></Pressable>
+                <Pressable onPress={() => openModal('depositar')}><Text style={styles.botao}>Depositar</Text></Pressable>
             </View>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={isModalOpen}
+              onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+                setIsModalOpen(!isModalOpen);
+              }}>
+              <View style={styles.centraliza}>
+                <View style={styles.modal}>
+                  <Pressable 
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setIsModalOpen(!isModalOpen)}><Text>X</Text></Pressable>
+                  <Text>Valor Atual: R${number.toFixed(2)}</Text>
+                  <Text>Valor Final: R${(operationType === 'sacar' ? number - (Number(valor) * 1.025) : number + (Number(valor) * 1.01)).toFixed(2)}</Text>
+                  <Text>Tem certeza que deseja {operationType === 'sacar' ? 'sacar' : 'depositar'}?</Text>
+                  <View style={styles.alinha}> 
+                  <Pressable 
+                    onPress={() => operacao(operationType)}><Text style={styles.confirm}>Sim</Text></Pressable>
+                  <Pressable 
+                    onPress={() => setIsModalOpen(false)}><Text style={styles.confirm}>Não</Text></Pressable>
+                  </View>
+                </View>
+              </View>
+          </Modal>
         </View>
     );
 };
@@ -56,10 +85,10 @@ const styles = StyleSheet.create({
     logo: {
         width: 200,
         height: 200,
-        marginBottom: 20
+        marginBottom: 0
     },
     saldo: {
-        margin: 30
+        margin: 20
     },
 
     alinha: {
@@ -91,6 +120,47 @@ const styles = StyleSheet.create({
         color: '#FFF', 
         width: 150,
         margin: 10
+    },
+
+    centraliza: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 22,
+    },
+
+    modal: {
+      margin: 20,
+      backgroundColor: '#fff',
+      borderRadius: 20,
+      padding: 35,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 1,
+        height: 6,
+      },
+      shadowOpacity: 0.60,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+
+    buttonClose: {
+        alignSelf: 'flex-end',
+        fontSize: 10,
+        marginBottom: 20,
+        
+    },
+
+    confirm: {
+      backgroundColor: '#1891C8',
+      marginTop: 25,
+      marginHorizontal: 10,
+      width: 50,
+      borderRadius: 2,
+      color: '#FFF', 
+      textAlign: 'center',
+      padding: 2
     }
 });
 
