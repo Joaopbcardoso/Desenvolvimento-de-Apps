9ca65b522ac37fa1961a5b111e31f0d755ef33b8
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-09-16 12:30:34 -0300

    menu

---
+++ app/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { View, Text, Pressable, Image, StyleSheet, FlatList, SafeAreaView, ScrollView } from "react-native";
+import { Link } from "expo-router";
+
+export default function Home() {
+    const data = [
+        { id: '1', link: './banco/Banco', titulo: 'Benvenutti Association' },
+        { id: '2', link: '/calculadora/Calculadora', titulo: 'Calculadora' },
+        { id: '3', link: '/lista-tarefa/Lista', titulo: 'Lista de Tarefa' },
+        { id: '4', link: '/login/Login', titulo: 'Atividade do Login' },
+        { id: '5', link: '/picker/Pokemon', titulo: 'App do Pokemón' },
+        { id: '6', link: '/splash-screen/SplashScreen', titulo: 'Splash Screen Disney+' },
+    ];
+    
+
+    return (
+        <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+            <Image
+                style={styles.logo}
+                source={require('./desenvolvimento-de-apps.png')}
+            />
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <Link style={styles.link} href={item.link}>
+                        {item.titulo}
+                    </Link>
+                )}
+            />
+        </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+
+    scrollView: {
+        width: '100%'
+      },
+
+    logo: {
+        width: 400,
+        height: 400,
+    },
+    link:{
+        margin: 20,
+        padding: 4,
+        borderColor: '#000'
+    }
+});
