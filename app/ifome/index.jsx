334a623cd6c8141655fd3756ac920016ea808f77
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-09-30 12:28:31 -0300

    usecontext

---
+++ app/ifome/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text, Image, StyleSheet, createContext } from 'react-native';
+
+const cartContext = createContext()
+
+export default function Home(){
+    return(
+        <View>
+
+        </View>
+    )
+}
